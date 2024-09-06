339e254b0b86056ee1b84a3b43561313440f8f13
Alex Sudakov <alexandr.sudakov@uber.com>
2024-09-07 00:00:06 +0200

    unhardcode home dir for lua

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -2,7 +2,7 @@ require("obsidian").setup({
   workspaces = {
     {
       name = "Notes",
-      path = "/Users/alexandr.sudakov/Obsidian/Notes",
+      path = os.getenv("HOME") .. "/Obsidian/Notes",
     },
   },
 })
