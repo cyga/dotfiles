11e08159212bf78658a27702c3bfb2bcca96e318
Alexandr Sudakov <cygakoB@gmail.com>
2024-09-14 22:45:29 +0200

    scrolling for lsp

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -174,6 +174,16 @@ local plugins = {
             opts = { skip = true },
           }
         },
+        vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+          if not require("noice.lsp").scroll(4) then
+            return "<c-f>"
+          end
+        end, { silent = true, expr = true }),
+        vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+          if not require("noice.lsp").scroll(-4) then
+            return "<c-b>"
+          end
+        end, { silent = true, expr = true }),
       })
     end,
     dependencies = {
